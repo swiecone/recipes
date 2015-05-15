422bdd6eb65cc7bd182039e091860f04292a8db2
swiecone <alejandro.swiec@gmail.com>
2015-05-15 22:40:10 +0100

    Added the like method in the recipe controller to show a flash when clicking in like/dislike and modified the show to pass the parameter *like* as another parameter in the post

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -40,7 +40,10 @@ def update
 end 
 
 def like
-
+	@recipe = Recipe.find(params[:id])
+	Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+	flash[:success] = "Your selection was successful"
+	redirect_to :back
 end
 
 
