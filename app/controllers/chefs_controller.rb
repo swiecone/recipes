b2726e6496fa09014cdbb904aacb491bedba6348
swiecone <alejandro.swiec@gmail.com>
2015-05-22 07:57:14 +0100

    Added the login functionality to the recipes web app

--- app/controllers/chefs_controller.rb
+++ app/controllers/chefs_controller.rb
@@ -1,7 +1,45 @@
 class ChefsController < ApplicationController
 
+	def index
+			@chefs = Chef.paginate(page: params[:page], per_page: 3)
+	end
+
 	def new
-		@chef = Chef.create()
+		@chef = Chef.new
 	end 
 
+	def create
+		@chef = Chef.new(chef_params)
+		if @chef.save
+			flash[:success] = "Your account has been created!"
+			redirect_to recipes_path
+		else
+			render 'new'
+		end
+	end 
+
+	def edit 
+		@chef = Chef.find(params[:id])
+	end 
+
+	def update
+		@chef = Chef.find(params[:id])
+		if @chef.update(chef_params)
+			flash[:success] = "your profile has been updated successfully"
+			redirect_to recipes_path #to do change to show page!!
+		else
+			render 'edit' 
+		end
+	end
+
+	def show 
+		@chef = Chef.find(params[:id])	
+		@recipes = @chef.recipes.paginate(page: params[:page], per_page: 3)
+
+	end 
+
+private
+	def chef_params
+			params.require(:chef).permit(:chefname, :email, :password)
+	end
 end 
--- app/models/chef.rb
+++ app/models/chef.rb
@@ -2,7 +2,9 @@ class Chef < ActiveRecord::Base
 	has_many :recipes
 	has_many :likes
 	before_save { self.email = email.downcase }
-	validates :chefname, presence: true, length: { minimum: 3, maximum: 40}
+	validates :chefname, presence: true, length: { minimum: 3, maximum: 40}, 
+				uniqueness: { case_sensitive: false }
+
 	VALID_EMAIL_REGEX = /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 	validates :email, presence: true, length: {maximum: 105},
 							uniqueness: { case_sensitive: false },
